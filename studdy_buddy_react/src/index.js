6885e752ef938ca53b26c6bc3f83af6747b0e719
dchege711 <d.chege711@gmail.com>
2018-01-28 15:03:33 -0500

    Set up barebones for logging in.

---
+++ studdy_buddy_react/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './w3.css';
+
+class LogIn extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Working on the Log In Page</p>
+            </div>
+        )
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <LogIn />,
+  document.getElementById('root')
+);
