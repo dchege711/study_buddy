ad00f93e2b33c4f82c3933e85a4d57e4d51c6984
dchege711 <d.chege711@gmail.com>
2018-01-28 18:54:01 -0500

    Reorganize the folders

--- study_buddy_react/src/index.js
+++ study_buddy_react/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/w3.css';
 
-import LogIn from './templates/LogIn.js'
+import LogIn from './LogIn.js'
 
 // ========================================
 
