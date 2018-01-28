2a967a2a7c447c7fda7a2cfc7189e30a453cb015
dchege711 <d.chege711@gmail.com>
2018-01-28 17:51:36 -0500

    Implement barebones log in.

--- studdy_buddy_react/src/index.js
+++ studdy_buddy_react/src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './w3.css';
+import './css/w3.css';
+// const logInTemplate = require('./templates/logInTemplate')
 
 class LogIn extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
+        // Binding is necessary to make this work in the callback
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleInputChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    
+    handleSubmit(event) {
+        // To-Do: Validate that the user is legit
+        event.preventDefault();
+    }
+    
     render() {
         return (
-            <div>
-                <p>Working on the Log In Page</p>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+            
+                <label>Username
+                    <input type="text" name="username" value={this.state.username} 
+                    placeholder="Enter Username" onChange={this.handleInputChange} />
+                </label>
+                
+                <label>Password
+                    <input type="password" name="password" value={this.state.password} 
+                    placeholder="Enter Password" onChange={this.handleInputChange} />
+                </label>
+                
+                <input type="submit" value="Submit" />
+                
+            </form>
         )
     }
 }
@@ -15,6 +51,6 @@ class LogIn extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <LogIn />,
-  document.getElementById('root')
+    <LogIn />,
+    document.getElementById('root')
 );
