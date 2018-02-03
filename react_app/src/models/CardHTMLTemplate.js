6074326efeefe8731bbeecde6d70db7d678a68b1
dchege711 <d.chege711@gmail.com>
2018-02-03 15:33:09 -0500

    :grin: I guess you can call me DJ Stylez :-P

--- react_app/src/models/CardHTMLTemplate.js
+++ react_app/src/models/CardHTMLTemplate.js
@@ -6,12 +6,12 @@ function CardHTMLTemplate(props) {
             
             <div className="w3-container w3-right">
                 <button onClick={props.resetContents}>
-                    <i className="fa fa-plus-square-o fa-2x"></i>
+                    <i className="fa fa-plus-square-o fa-fw"></i>Add New Card
                 </button>
             </div>
         
-            <div className="w3-blue w3-container">
-                <label>
+            <div className="w3-blue w3-center w3-container">
+                <label style={{width:"70%", padding:"10px 5px 20px 5px"}}>
                     <input type="text" name="title" value={props.title} style={{width:"inherit"}}
                     className="w3-input w3-threequarter" onChange={props.handleInputChange} />
                 </label>
@@ -19,21 +19,21 @@ function CardHTMLTemplate(props) {
             
             <div className="w3-container">
         
-                <label><strong>Description</strong>
+                <label className="input-area"><strong>Description</strong>
                     <textarea name="description" value={props.description}
                     className="w3-input" onChange={props.handleInputChange} />
                 </label>
                 
                 <br />
                 
-                <label><strong>Tags</strong>
+                <label className="input-area"><strong>Tags</strong>
                     <textarea name="tags" value={props.tags}
                     className="w3-input" onChange={props.handleInputChange} />
                 </label>
                 
                 <br />
             
-                <label><strong>Urgency</strong>
+                <label className="input-area"><strong>Urgency</strong>
                     <input type="number" name="urgency" className="w3-input" 
                     value={props.urgency} onChange={props.handleInputChange} />
                 </label>
@@ -41,7 +41,7 @@ function CardHTMLTemplate(props) {
             </div>
         
             <div className="w3-container">
-                <input type="submit" name="update" className="w3-input"
+                <input type="submit" name="update" className="w3-input w3-center"
                     value={props.submitLabel} onClick={props.handleSubmit} 
                 />
                 
