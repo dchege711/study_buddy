88566c080e1307967b7d60ca5b12005e0298b9f2
dchege711 <d.chege711@gmail.com>
2018-02-15 22:17:36 -0500

    Try running npm start instead of npm run build

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var CardController = require('./react_app/src/controllers/CardController');
 
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
