6d29f60ff6820c3f93213c331ea633d1aa6033aa
dchege711 <d.chege711@gmail.com>
2018-03-24 22:06:18 -0400

    Run the server and the client concurrently in bash

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,3 @@
 node server.js &
-cd client_side_react && npm install --only=dev && npm install && npm run build &
+# cd client_side_react && npm install --only=dev && npm install && npm run build &
+cd client_side_react && npm start &
