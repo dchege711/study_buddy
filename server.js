51e5b3bf738f51145cf47bfbf62d0ecb49cc58cd
dchege711 <d.chege711@gmail.com>
2018-08-13 17:33:52 -0400

    Revert HTTPS changes

--- server.js
+++ server.js
@@ -29,16 +29,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-app.use(function (req, res, next) {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-    } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-});
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
