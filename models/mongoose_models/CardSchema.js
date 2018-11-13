4e105a5d20f6366efd8bd7b780c70b82a4fe77cf
dchege711 <d.chege711@gmail.com>
2018-11-13 16:41:20 -0500

    Add fields necessary for public/private cards.

--- models/mongoose_models/CardSchema.js
+++ models/mongoose_models/CardSchema.js
@@ -18,11 +18,15 @@ var cardSchema = new mongoose.Schema(
         title: { type: String, default: "" },
         description: { type: String, default: "" },
         tags: { type: String, lowercase: true, trim: true, default: "" },
-        urgency: { type: Number, default: 0 },
-        metadataIndex: Number,
-        createdById: Number,
+        urgency: { type: Number, default: 10 },
+        metadataIndex: { type: Number, default: 0 },
+        createdById: { type: Number, required: true}, 
         isPublic: { type: Boolean, default: false },
-        lastReviewed: { type: Date, default: Date.now }
+        lastReviewed: { type: Date, default: Date.now },
+        parent: { type: String, default: "" },
+        numChildren: { type: Number, default: 0 },
+        numTimesMarkedAsDuplicate: { type: Number, default: 0 },
+        numTimesMarkedForReview: { type: Number, default: 0 }
     }, 
     {
         timestamps: true,
