e0c220443366a8b29f045566254f95e6a58a0524
dchege711 <d.chege711@gmail.com>
2018-11-13 16:38:13 -0500

    Fail gracefully when a status is not provided to the last handler.

--- controllers/ControllerUtilities.js
+++ controllers/ControllerUtilities.js
@@ -14,20 +14,22 @@ const generic_500_msg = {
  * @param {Response} res An Express JS res object 
  */
 exports.convertObjectToResponse = function (err, result_JSON, res) {
-    let status = result_JSON.status;
     if (err) {
         res.render(
             "pages/5xx_error_page.ejs", 
             { response_JSON: generic_500_msg, appName: appName }
         );
-    } else if (status >= 200 && status < 300) {
-        res.json(result_JSON);
-    } else if (status >= 300 && status < 400) {
-        res.redirect(status, result_JSON.redirect_url + "?msg=" + encodeURIComponent(result_JSON.message));
-    } else if (status >= 400 && status < 500) {
-        res.render("pages/4xx_error_page.ejs", { response_JSON: result_JSON, appName: appName });
     } else {
-        res.render("pages/5xx_error_page.ejs", { response_JSON: result_JSON, appName: appName });
+        let status = result_JSON.status;
+        if (status >= 200 && status < 300) {
+            res.json(result_JSON);
+        } else if (status >= 300 && status < 400) {
+            res.redirect(status, result_JSON.redirect_url + "?msg=" + encodeURIComponent(result_JSON.message));
+        } else if (status >= 400 && status < 500) {
+            res.render("pages/4xx_error_page.ejs", { response_JSON: result_JSON, appName: appName });
+        } else {
+            res.render("pages/5xx_error_page.ejs", { response_JSON: result_JSON, appName: appName });
+        }
     }
 };
 
