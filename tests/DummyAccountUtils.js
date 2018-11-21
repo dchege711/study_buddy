5179e43e8401a5fe720f129eaae1fdad8f1369a1
dchege711 <d.chege711@gmail.com>
2018-11-20 19:39:12 -0500

    Add a utility method for creating a dummy account with 60 cards

--- tests/DummyAccountUtils.js
+++ tests/DummyAccountUtils.js
@@ -2,16 +2,18 @@
 
 const LoginUtilities = require("../models/LogInUtilities.js");
 const config = require("../config.js");
+const sampleCards = require("./SampleCards.js");
+const CardsDB = require("../models/CardsMongoDB.js");
 
 /**
  * @returns {Promise} resolves with a JSON representation of a logged in user.
- * 
  */
 exports.getDummyAccount = function() {
     let dummyAccountDetails = {
         username: config.DEBUG_USERNAME, password: config.DEBUG_PASSWORD,
         email: config.DEBUG_EMAIL_ADDRESS
     }
+
     return new Promise(function(resolve, reject) {
         LoginUtilities
             .registerUserAndPassword(dummyAccountDetails)
@@ -28,4 +30,29 @@ exports.getDummyAccount = function() {
             .catch((err) => { reject(err); });
     });
 };
+
+if (require.main === module) {
+
+    const dbConnection = require("../models/MongooseClient.js");
+
+    // Only exit once done is set to true...
+    // var done = (function wait () { if (!done) setTimeout(wait, 1000) })();
+    var done = true;
+
+    exports
+        .getDummyAccount()
+        .then(async (loggedInUser) => {
+            let cards = sampleCards.getRandomCards(60);
+            for (let i = 0; i < cards.length; i++) {
+                let card = cards[i];
+                card.createdById = loggedInUser.userIDInApp;
+                if (i % 3 == 0) card.isPublic = false;
+                else card.isPublic = true;
+                await CardsDB.create(card);
+                console.log(`Added ${card.title}`);
+            }
+            done = true;
+        })
+        .catch((err) => { console.error(err); done = true; });
+}
 
