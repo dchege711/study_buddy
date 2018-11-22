475c686f6a4b77be6866a7938a333edf8ef676d6
dchege711 <d.chege711@gmail.com>
2018-11-22 12:26:39 -0500

    Report the number of tests passed. Exit with non-zero code if need be

--- tests/selenium_tests/Test.js
+++ tests/selenium_tests/Test.js
@@ -1,11 +1,36 @@
-const TestSignUpFlow = require("./TestSignUpFlow.js");
+"use strict";
 
-async function runTestSuite() {
+const TestSignUpFlow = require("./TestSignUpFlow.js");
+const LoginUtils = require("../../models/LogInUtilities.js");
+const dbConnection = require("../../models/MongooseClient.js");
+
+let headless = process.argv[2] === "headless";
+
+async function runTestSuite(headless=true) {
+
+    let passedAllTests = true;
+    let numDeleted = await LoginUtils.deleteAllAccounts();
+    console.log(`\n${numDeleted} account(s) were deleted\n`);
+
+
+    console.log(`Testing signup paths...`);
     await TestSignUpFlow
-        .test()
+        .test(headless=headless)
+        .then(([numTestsPassed, numTotalTests]) => {
+            console.log(`\n${numTestsPassed}/${numTotalTests} tests passed!`);
+            passedAllTests = numTestsPassed === numTotalTests;
+        })
         .catch((err) =>{ 
             console.error(err); 
         });
+
+    return Promise.resolve(passedAllTests);
 }
 
-runTestSuite();
+runTestSuite(headless=headless)
+    .then((passedAllTests) => {
+        // Dear Future Maintainer: I know, I'm so sorry...
+        if (!passedAllTests) process.exit(1);
+        else process.exit(0);
+    })
+    .catch((err) => { console.error(err); process.exit(1); });
