935773243247ce968befc88d3cbe5deceed16c32
dchege711 <d.chege711@gmail.com>
2018-11-22 23:51:17 -0500

    Silence depracation warnings when running test suites

--- tests/selenium_tests/run_selenium_tests.sh
+++ tests/selenium_tests/run_selenium_tests.sh
@@ -9,22 +9,24 @@ function cleanup {
 cleanup # Clean the workspace before running anything
 trap cleanup EXIT # Run the cleanup routine before exiting this script
 
-node ../../server.js & # Spin up the server
+node_with_options="node --no-deprecation"
+
+$node_with_options ../../server.js & # Spin up the server
 sleep 1.5 # Enough time for the server to initialize
 
 browser_to_test=`echo "$1" | tr '[:upper:]' '[:lower:]'`
 
 if [[ $browser_to_test == "all" ]] || [[ $browser_to_test == "chrome" ]]; then
     echo -e "\n__________________\n\nTesting on chrome...\n__________________\n"
-    SELENIUM_SERVER_JAR=./selenium-server-standalone-3.8.1.jar SELENIUM_BROWSER=chrome node Test.js $2
+    SELENIUM_SERVER_JAR=./selenium-server-standalone-3.8.1.jar SELENIUM_BROWSER=chrome $node_with_options Test.js $2
 fi
 
 if [[ $browser_to_test == "all" ]] || [[ $browser_to_test == "safari" ]]; then
     echo -e "\n__________________\n\nTesting on safari...\n__________________\n"
-    SELENIUM_SERVER_JAR=./selenium-server-standalone-3.8.1.jar SELENIUM_BROWSER=safari node Test.js $2
+    SELENIUM_SERVER_JAR=./selenium-server-standalone-3.8.1.jar SELENIUM_BROWSER=safari $node_with_options Test.js $2
 fi
 
 if [[ $browser_to_test == "all" ]] || [[ $browser_to_test == "firefox" ]]; then
     echo -e "\n__________________\n\nTesting on firefox...\n__________________\n"
-    SELENIUM_SERVER_JAR=./selenium-server-standalone-3.8.1.jar SELENIUM_BROWSER=firefox node Test.js $2
+    SELENIUM_SERVER_JAR=./selenium-server-standalone-3.8.1.jar SELENIUM_BROWSER=firefox $node_with_options Test.js $2
 fi
