855ee75d931dae1e7613303de913bb3e38ec8716
dchege711 <d.chege711@gmail.com>
2018-12-24 18:28:26 -0500

    Modularize and expose the method for populating the dummy account

--- tests/DummyAccountUtils.js
+++ tests/DummyAccountUtils.js
@@ -4,6 +4,7 @@ const LoginUtilities = require("../models/LogInUtilities.js");
 const config = require("../config.js");
 const sampleCards = require("./SampleCards.js");
 const CardsDB = require("../models/CardsMongoDB.js");
+const Misc = require("../models/Miscellaneous.js");
 
 /**
  * @returns {Promise} resolves with a JSON representation of a logged in user.
@@ -15,8 +16,13 @@ exports.getDummyAccount = function() {
     }
 
     return new Promise(function(resolve, reject) {
-        LoginUtilities
-            .registerUserAndPassword(dummyAccountDetails)
+        LoginUtilities.deleteAllAccounts()
+            .then((_) => {
+                return Misc.addPublicUser();
+            })
+            .then((_) => {
+                return LoginUtilities.registerUserAndPassword(dummyAccountDetails);
+            })
             .then((_) => {
                 return LoginUtilities.authenticateUser({
                     username_or_email: dummyAccountDetails.username,
@@ -31,23 +37,39 @@ exports.getDummyAccount = function() {
     });
 };
 
+/**
+ * @description Creates the dummy account if it doesn't exist. Populates the 
+ * dummy account with `numCards` cards.
+ * @returns {Promise} resolves with a JSON representation of a logged in user.
+ */
+exports.populateDummyAccount = function(numCards=50) {
+    return new Promise(function(resolve, reject) {
+        exports.getDummyAccount()
+            .then(async (loggedInUser) => {
+                let cards = sampleCards.getRandomCards(numCards);
+                for (let i = 0; i < cards.length; i++) {
+                    let card = cards[i];
+                    card.createdById = loggedInUser.userIDInApp;
+                    if (i % 3 == 0) card.isPublic = false;
+                    else card.isPublic = true;
+                    await CardsDB.create(card);
+                }
+                resolve(loggedInUser)
+            })
+            .catch((err) => { reject(err); });
+    }); 
+}
+
 if (require.main === module) {
 
     const dbConnection = require("../models/MongooseClient.js");
-
-    exports.getDummyAccount()
-        .then(async (loggedInUser) => {
-            let cards = sampleCards.getRandomCards(50);
-            for (let i = 0; i < cards.length; i++) {
-                let card = cards[i];
-                card.createdById = loggedInUser.userIDInApp;
-                if (i % 3 == 0) card.isPublic = false;
-                else card.isPublic = true;
-                await CardsDB.create(card);
-                console.log(`Added ${card.title}`);
-            }
+    let numCards = 60;
+    exports.populateDummyAccount(numCards)
+        .then((user) => { 
+            console.log(`Created user ${user.userIDInApp} and gave them ${numCards} cards`);
+            return dbConnection.closeMongooseConnection(); 
         })
-        .then(() => { return dbConnection.closeMongooseConnection(); })
         .catch((err) => { console.error(err); });
+        
 }
 
