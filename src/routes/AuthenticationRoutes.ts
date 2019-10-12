7810b0d6df5c5d8c633ff5437fdb5a9a498d45ae
dchege711 <d.chege711@gmail.com>
2019-10-11 23:45:47 -0700

    Port AuthenticationRoutes to TS & add a unit test

---
+++ src/routes/AuthenticationRoutes.ts
@@ -0,0 +1,42 @@
+/**
+ * @description Provide routes related to authenticating users.
+ */
+
+import { Router } from "express";
+
+import { 
+    handleLogIn, registerUser, loginUser, logoutUser, sendValidationEmailGet, 
+    sendValidationEmailPost, verifyAccount, resetPasswordGet, resetPasswordPost, 
+    resetPasswordLinkGet, resetPasswordLinkPost 
+} from "../controllers/AuthenticationController"
+
+/**
+ * A AuthenticationRouter with routes for registering users, logging in users, processing 
+ * account validation emails and processing password resets.
+ */
+const AuthenticationRouter = Router();
+
+AuthenticationRouter.get("/", handleLogIn);
+AuthenticationRouter.get("/login", handleLogIn);
+
+AuthenticationRouter.post("/register-user", registerUser);
+
+AuthenticationRouter.post("/login", loginUser);
+
+AuthenticationRouter.post("/logout", logoutUser);
+
+AuthenticationRouter.get("/send-validation-email", sendValidationEmailGet);
+
+AuthenticationRouter.post("/send-validation-email", sendValidationEmailPost);
+
+AuthenticationRouter.get("/verify-account/*", verifyAccount);
+
+AuthenticationRouter.get("/reset-password", resetPasswordGet);
+
+AuthenticationRouter.post("/reset-password", resetPasswordPost);
+
+AuthenticationRouter.get("/reset-password-link/*", resetPasswordLinkGet);
+
+AuthenticationRouter.post("/reset-password-link/*", resetPasswordLinkPost);
+
+export { AuthenticationRouter };
---
+++ src/routes/AuthenticationRoutesUnitTest.ts
@@ -0,0 +1,24 @@
+import assert = require("assert");
+
+import { AuthenticationRouter } from "./AuthenticationRoutes";
+import { GET, POST, RouteChecker } from "./TestUtilsRouter";
+
+describe("AuthenticationRouter.ExpectedRoutes", function() {
+    it("should define routes for these URLs", function() {
+
+        const expectedRoutes = [
+            ["/", GET], ["/login", GET], ["/register-user", POST], 
+            ["/login", POST], ["/logout", POST], 
+            ["/send-validation-email", GET], ["/send-validation-email", POST], 
+            ["/verify-account/*", GET], ["/reset-password", GET], 
+            ["/reset-password", POST], ["/reset-password-link/*", GET], 
+            ["/reset-password-link/*", POST]
+        ];
+
+        const routeChecker = new RouteChecker(AuthenticationRouter);
+        expectedRoutes.forEach((routeAndMethod) => {
+            routeChecker.hasRoute(routeAndMethod[0], routeAndMethod[1]);
+        });
+
+    });
+});
