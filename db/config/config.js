89a9a12c6e398c70a1d9068362c079058dbc1319
dchege711 <d.chege711@gmail.com>
2020-02-22 18:02:52 -0800

    [DB] Record changes made to config file
    I feel that I did a lot of work here and I might want to look at it later.

--- db/config/config.js
+++ db/config/config.js
@@ -1,13 +1,42 @@
-const AppConfig = require("../../dist/config.js");
+/**
+ * @description Tells the CLI how to connnect to the database.
+ */
 
+const DB_URI = require("../../dist/config.js").DATABASE_URI;
+
+/**
+ * Extract the components from the connection URI. The form is defined as below:
+ * 
+ * `postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]`
+ * 
+ * For our use case, we expect the string to be as follows:
+ * 
+ * `postgres://[user]:[password]@[host]:[port]/[dbname]`;
+ * 
+ * https://www.postgresql.org/docs/current/libpq-connect.html
+ */
+const DB_URI_REGEX = /postgres:\/\/(\w+):(\w+)@(\w+):(\d+)\/(\w+)/;
+const uriComponents = DB_URI_REGEX.exec(DB_URI);
+
+if (uriComponents.length !== 5) {
+  throw new Error(`Expected 5 URI components, but received ${uriComponents}`);
+}
+
+let dbConnectionObject = {
+  username: uriComponents[0],
+  password: uriComponents[1],
+  host: uriComponents[2],
+  port: uriComponents[3],
+  database: uriComponents[4],
+  "dialect": "postgres",
+};
+
+/**
+ * @description The keys of the objects, e.g. `development`, are used in
+ * `model/index.js` for matching `process.env.NODE_ENV`.
+ */
 module.exports = {
-    "development": {
-      "username": "root",
-      "password": null,
-      "database": "database_development",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
-    },
+    "development": dbConnectionObject,
     "test": {
       "username": "root",
       "password": null,
@@ -15,11 +44,5 @@ module.exports = {
       "host": "127.0.0.1",
       "dialect": "postgres"
     },
-    "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
-    }
+    "production": dbConnectionObject
 }
