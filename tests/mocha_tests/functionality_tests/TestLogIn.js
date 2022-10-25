3dd170b2520879c9340487875301f9fda08e7f0c
Chege Gitau <d.chege711@gmail.com>
2022-10-25 00:49:19 -0700

    Revert "[LoginUtilitiesTest] Extend timeout to 5s to accomodate CI"
    This reverts commit b0595c037fe1a3eb1b9664f8ad92ad64746ffb46.
    
    There was no effect in CI. The test still failed [1].
    
    [1]: https://github.com/dchege711/study_buddy/actions/runs/3319071224/jobs/5483761162

--- tests/mocha_tests/functionality_tests/TestLogIn.js
+++ tests/mocha_tests/functionality_tests/TestLogIn.js
@@ -8,7 +8,6 @@ const config = require("../../../config.js");
 describe("Test LoginUtilities\n", function() {
 
     describe("#registerUserAndPassword()", function() {
-        this.timeout(5000); // These tests may run slower than 2s in CI.
 
         before(function() {
             return Miscellaneous.addPublicUser();
