6aacdbd044f0a647257b332fa3d2637ab87ddf4c
Chege Gitau <d.chege711@gmail.com>
2023-07-01 19:41:21 -0700

    [Graph] Make the undirected nature of the graph clear

--- public/src/Graph.js
+++ public/src/Graph.js
@@ -4,52 +4,50 @@ const MaxPQ = require("./MaxPriorityQueue.js");
 
 /**
  * @description A minimal graph data type
- * 
+ *
  * @class
  */
-function Graph(directed=false) {
+function UndirectedGraph() {
 
-    Graph.__adj = {};
-    this.__adj = Graph.__adj; // Future Me: I'm so sorry. Quick way to inspect the graph
+    UndirectedGraph.__adj = {};
+    this.__adj = UndirectedGraph.__adj; // Future Me: I'm so sorry. Quick way to inspect the graph
 
     /**
-     * @description Add edge `(nodeOneID, nodeTwoID)` into the graph and set its weight 
+     * @description Add edge `(nodeOneID, nodeTwoID)` into the graph and set its weight
      * to `weight`.
-     * 
+     *
      * @param nodeOneID the id of the first node
      * @param nodeTwoID the id of the second node
      * @param weight the weight to be assigned to the edge
-     * 
+     *
      */
     this.addEdge = function(nodeOneID, nodeTwoID, weight=1) {
 
-        if (Graph.__adj[nodeOneID] === undefined) Graph.__adj[nodeOneID] = {};
-        Graph.__adj[nodeOneID][nodeTwoID] = weight;
+        if (UndirectedGraph.__adj[nodeOneID] === undefined) UndirectedGraph.__adj[nodeOneID] = {};
+        UndirectedGraph.__adj[nodeOneID][nodeTwoID] = weight;
 
-        if (!directed) {
-            if (Graph.__adj[nodeTwoID] === undefined) Graph.__adj[nodeTwoID] = {};
-            Graph.__adj[nodeTwoID][nodeOneID] = weight;
-        }
+        if (UndirectedGraph.__adj[nodeTwoID] === undefined) UndirectedGraph.__adj[nodeTwoID] = {};
+        UndirectedGraph.__adj[nodeTwoID][nodeOneID] = weight;
     }
 
     /**
-     * @returns {Number} the weight assigned to the `(nodeOneID, nodeTwoID)` if 
+     * @returns {Number} the weight assigned to the `(nodeOneID, nodeTwoID)` if
      * the edge exists. If the edge doesn't exist, returns `null`.
      */
     this.getEdgeWeight = function(nodeOneID, nodeTwoID) {
-        if (Graph.__adj[nodeOneID] && Graph.__adj[nodeOneID][nodeTwoID]) {
-            return Graph.__adj[nodeOneID][nodeTwoID];
+        if (UndirectedGraph.__adj[nodeOneID] && UndirectedGraph.__adj[nodeOneID][nodeTwoID]) {
+            return UndirectedGraph.__adj[nodeOneID][nodeTwoID];
         } else {
             return null;
         }
     }
 
     /**
-     * @description Find `k` nodes that are near the input array of node IDs. 
-     * This is a rough estimate which doesn't guarantee global optimality. 
+     * @description Find `k` nodes that are near the input array of node IDs.
+     * This is a rough estimate which doesn't guarantee global optimality.
      * The search for the neighbors proceeds in a BFS manner.
-     * 
-     * @returns Map whose keys are the `k` nodes near to `nodeID` and their 
+     *
+     * @returns Map whose keys are the `k` nodes near to `nodeID` and their
      * values are the respective distances
      */
     this.kNearNeighbors = function(nodeIDs, k=7) {
@@ -58,16 +56,16 @@ function Graph(directed=false) {
         let pq = new MaxPQ();
 
         function __enqueue(node, currentPQ) {
-            if (!Graph.__adj[node]) return;
-            Object.keys(Graph.__adj[node]).forEach((otherNode) => {
+            if (!UndirectedGraph.__adj[node]) return;
+            Object.keys(UndirectedGraph.__adj[node]).forEach((otherNode) => {
                 if (!alreadySeenIDs.has(otherNode)) {
-                    currentPQ.insert([otherNode, Graph.__adj[node][otherNode] * -1]);
+                    currentPQ.insert([otherNode, UndirectedGraph.__adj[node][otherNode] * -1]);
                 }
             });
         }
 
         nodeIDs.forEach((nodeID) => { __enqueue(nodeID, pq); });
-        
+
         let nodeAndNegatedDistance, nextPQ;
         while (kNeighbors.size < k && !pq.is_empty()) {
             nextPQ = new MaxPQ();
@@ -87,4 +85,4 @@ function Graph(directed=false) {
 
 }
 
-module.exports = Graph;
+module.exports = UndirectedGraph;
