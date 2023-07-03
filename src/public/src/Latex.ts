359c82b73c634cf66f81ec5d64fa75951bdd9ba8
Chege Gitau <d.chege711@gmail.com>
2023-07-03 00:05:54 -0700

    [ES] Fix loading errors for latex and syntax highlighting
    These two resources are easier to consume on the browser-side.

--- src/public/src/Latex.ts
+++ src/public/src/Latex.ts
@@ -1,3 +1,4 @@
+// Will be overwritten in src/views/partials/syntax_highlighting_and_latex.ejs.
 let latexImplementation = (elementIDs: string[]) => {};
 
 export function renderLatex(elementIDs: string[]): void {
--- src/public/src/SyntaxHighlighting.ts
+++ src/public/src/SyntaxHighlighting.ts
@@ -1,15 +1,6 @@
-const hljs = require('./lib/highlight.pack.js');
+// Will be overwritten in src/views/partials/syntax_highlighting_and_latex.ejs.
+let addSyntaxHighlightingImplementation = () => {};
 
-/**
- * @description We found the [highlight.js]{@link https://highlightjs.org/}
- * library useful for syntax highlighting. It even allows us to specify
- * what languages we want supported. We downloaded a package from them
- * instead of using a CDN. Although we refrain from adding more
- * dependencies, writing our own syntax highlighter would have been unwise.
- */
-export function addSyntaxHighlighting () {
-  let code_elements = document.querySelectorAll('pre code');
-  for (let i = 0; i < code_elements.length; i++) {
-      hljs.highlightBlock(code_elements[i]);
-  }
+export function addSyntaxHighlighting() {
+  addSyntaxHighlightingImplementation();
 }
