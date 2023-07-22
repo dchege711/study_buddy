6e1cf20a4b460cb2ac788ebc319a6c3aeaa2495c
Chege Gitau <d.chege711@gmail.com>
2023-07-22 12:44:21 -0700

    [Footer] Blend CSS with the rest of the page

--- src/public/src/partials/Footer.tsx
+++ src/public/src/partials/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="w3-container w3-container w3-black">
+    <footer className="w3-container">
       <p className="w3-center">
         &#169; c13u {new Date(Date.now()).getFullYear()}
       </p>
