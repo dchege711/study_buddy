e1dfe01b2dd8a4b35636e89d0076d26da69bb29a
Chege Gitau <d.chege711@gmail.com>
2024-04-21 10:49:40 -0700

    [ES] Separate webpack.config for client and server

---
+++ tools/webpack.esloader.config.cjs
@@ -0,0 +1,50 @@
+const path = require("path");
+
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
+module.exports = (env, args) => {
+    const plugins = [
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
+            onStart() {
+                console.log('circular-dependency-plugin: looking for webpack modules cycles...');
+            }
+        })
+    ]
+
+    const mode = process.env.NODE_ENV || 'production';
+
+    return {
+        context: args.context,
+        mode,
+        devtool: mode === 'production' ? 'hidden-source-map' : 'source-map',
+        plugins,
+        resolve: {
+            extensions: ['.ts', '.js'],
+            extensionAlias: {
+                '.js': ['.ts', '.js'],
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: {
+                        loader: 'esbuild-loader',
+                        options: {
+                            format: 'esm',
+                            target: 'esnext',
+                        }
+                    }
+                }
+            ]
+        },
+        output: {
+            path: path.resolve(args.context, 'dist/'),
+            filename: '[name].js',
+            publicPath: '/dist/',
+            clean: true,
+        }
+    }
+}
