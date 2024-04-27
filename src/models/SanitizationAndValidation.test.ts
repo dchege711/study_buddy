7c313cffae705da88255541a9064b38cefe053c0
Chege Gitau <d.chege711@gmail.com>
2024-04-27 14:38:37 -0700

    [SanitizationAndValidation] More tests

--- src/models/SanitizationAndValidation.test.ts
+++ src/models/SanitizationAndValidation.test.ts
@@ -16,14 +16,43 @@ describe('HTML Transformations', function () {
     );
   });
 
-  it('should highlight code blocks', function() {
-    // We use Showdown to convert markdown to HTML, which doesn't support the
-    // language hint syntax for code blocks.
-    const result = sanitizeCard({
-      description: '```js\nlet x = 1;\nconst y = 2;\n```',
+  describe('Syntax Highlighting', function () {
+    // Helper value for identifying highlighted code blocks.
+    const hljsSentinel = '<pre><code data-highlighted="yes" class="hljs';
+
+    it('should highlight code blocks with a language hint', function () {
+      const result = sanitizeCard({
+        description: '```js\nlet x = 1;\nconst y = 2;\n```',
+      });
+      expect(result.descriptionHTML).satisfies(function (s: string) {
+        return s.startsWith(hljsSentinel);
+      }, result.descriptionHTML);
     });
-    expect(result.descriptionHTML).satisfies(function (s: string) {
-      return s.startsWith('<pre><code data-highlighted="yes" class="hljs');
+
+    it('should highlight code blocks without a language hint', function () {
+      const result = sanitizeCard({
+        description: '```\nlet x = 1;\nconst y = 2;\n```',
+      });
+      expect(result.descriptionHTML).satisfies(function (s: string) {
+        return s.startsWith(hljsSentinel);
+      }, result.descriptionHTML);
+    });
+
+    it('should not highlight inline code', function () {
+      const result = sanitizeCard({
+        description: 'This is `inline code`',
+      });
+      expect(result.descriptionHTML).to.eq('<p>This is <code>inline code</code></p>');
+    });
+
+    it('should limit highlighting to code blocks', function () {
+      const result = sanitizeCard({
+        description: '```js\nlet x = 1;\n```\nSome `inline code` followed by a code block\n```js\nlet y = 2;\n```',
+      });
+      expect(result.descriptionHTML).satisfies(function (s: string) {
+        return s.startsWith(hljsSentinel);
+      }, result.descriptionHTML);
+      expect(result.descriptionHTML).to.include(`<p>Some <code>inline code</code> followed by a code block</p>\n${hljsSentinel}`);
     });
   });
 });
