512bd5cb4ba5b3ccf0320f369c5ff423ec849998
Chege Gitau <d.chege711@gmail.com>
2024-04-27 23:09:50 -0700

    [LaTeX] SSR of Latex using KaTeX

--- src/models/SanitizationAndValidation.test.ts
+++ src/models/SanitizationAndValidation.test.ts
@@ -3,22 +3,18 @@ import { expect } from 'chai';
 import { sanitizeCard } from './SanitizationAndValidation.js';
 
 describe('HTML Transformations', function () {
-  /**
-   * @description Test that the function converts markdown to HTML. We use
-   * Showdown to convert markdown to HTML. No need for extensive testing here.
-   */
   it('should convert markdown to HTML', function () {
     const result = sanitizeCard({
       description: 'This is a **bold** text',
     });
     expect(result.descriptionHTML).to.eq(
-      '<p>This is a <strong>bold</strong> text</p>'
+      '<p>This is a <strong>bold</strong> text</p>\n'
     );
   });
 
   describe('Syntax Highlighting', function () {
     // Helper value for identifying highlighted code blocks.
-    const hljsSentinel = '<pre><code data-highlighted="yes" class="hljs';
+    const hljsSentinel = '<pre><code class="hljs';
 
     it('should highlight code blocks with a language hint', function () {
       const result = sanitizeCard({
@@ -39,10 +35,10 @@ describe('HTML Transformations', function () {
     });
 
     it('should not highlight inline code', function () {
-      const result = sanitizeCard({
+      const { descriptionHTML } = sanitizeCard({
         description: 'This is `inline code`',
       });
-      expect(result.descriptionHTML).to.eq('<p>This is <code>inline code</code></p>');
+      expect(descriptionHTML).to.eq('<p>This is <code>inline code</code></p>\n');
     });
 
     it('should limit highlighting to code blocks', function () {
@@ -53,6 +49,30 @@ describe('HTML Transformations', function () {
         return s.startsWith(hljsSentinel);
       }, result.descriptionHTML);
       expect(result.descriptionHTML).to.include(`<p>Some <code>inline code</code> followed by a code block</p>\n${hljsSentinel}`);
+    });
+  });
+
+  describe('LaTeX', function () {
+    it('should render LaTeX text', function () {
+      const { descriptionHTML } = sanitizeCard({
+        description: String.raw`$$e = mc^2$$`,
+      });
+      expect(descriptionHTML, descriptionHTML).to.include('<span class="katex">');
+    });
+
+    it('should not affect non-LaTeX text', function () {
+      const description = 'This string has no math in it.';
+      const { descriptionHTML } = sanitizeCard({ description });
+      expect(descriptionHTML, descriptionHTML).to.eq(`<p>${description}</p>\n`);
+    });
+
+    it('should not affect non-LaTeX parts of the text', function () {
+      const { descriptionHTML } = sanitizeCard({
+        description: String.raw`Einstein theorized \(e = mc^2\) in his paper.`,
+      });
+      expect(descriptionHTML).satisfies(function (s: string) {
+        return s.startsWith(`<p>Einstein theorized <eq><span class="katex">`);
+      }, descriptionHTML);
     });
   });
 });
