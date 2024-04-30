4be52bdc0756a98fa0ac75acaea8fcb592f6c5de
Chege Gitau <d.chege711@gmail.com>
2024-04-30 08:08:41 -0700

    [browse-page] Provide the CardsCarousel context

--- src/public/src/pages/browse-page.ts
+++ src/public/src/pages/browse-page.ts
@@ -2,6 +2,8 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { provide } from '@lit/context';
 
+import { CardsCarouselUpdateCursorDirection, cardsCarouselContext, kCardsCarouselUpdateCursorEventName } from '../context/cards-carousel-context.js';
+import { CardsCarousel, CardsCarouselBSTKey } from '../CardsCarousel.js';
 import { kSearchResultsChangedEventName, searchResultsContext, SearchResultsChangedEvent, kSearchResultSelectedEventName, SearchResultSelectedEvent } from '../context/search-results-context.js';
 import { trpc, CardSearchResult, PublicCardResult } from '../trpc.js';
 
@@ -14,6 +16,9 @@ export class BrowsePage extends LitElement {
   @provide({ context: searchResultsContext })
   @state() private searchResults: CardSearchResult[] = [];
   @state() private selectedResult: PublicCardResult = null;
+
+  @provide({ context: cardsCarouselContext })
+  @state() private cardsCarousel = new CardsCarousel([]);
 
   @property({ type: Boolean }) isPrivateSearch = false;
 
@@ -33,18 +38,41 @@ export class BrowsePage extends LitElement {
   private addEventListeners() {
     this.addEventListener(kSearchResultsChangedEventName, (ev) => {
       this.searchResults = ev.results;
+      this.cardsCarousel = new CardsCarousel(
+        ev.results.map(result => {
+          return {
+            _id: result._id!,
+            urgency: result.urgency!,
+          };
+        })
+      );
     });
 
     this.addEventListener(kSearchResultSelectedEventName, (ev) => {
-      let cardID = ev.result._id;
-      if (!cardID) {
-        return;
-      }
-
-      trpc.fetchPublicCard.query({ cardID }).then((card) => {
-        this.selectedResult = card;
-      });
+      this.updateSelectedCard(ev.result._id!);
     });
+
+    this.addEventListener(kCardsCarouselUpdateCursorEventName, (ev) => {
+      switch (ev.direction) {
+        case CardsCarouselUpdateCursorDirection.Next:
+          if (this.cardsCarousel.hasNext()) this.cardsCarousel.next();
+          break;
+        case CardsCarouselUpdateCursorDirection.Previous:
+          if (this.cardsCarousel.hasPrevious()) this.cardsCarousel.previous();
+          break;
+      }
+      let currentCard = this.cardsCarousel.current();
+      if (currentCard) {
+        this.updateSelectedCard(currentCard._id);
+      }
+    });
+  }
+
+  private updateSelectedCard(cardID: string) {
+    trpc.fetchPublicCard.query({ cardID }).then((card) => {
+      this.selectedResult = card;
+    });
+
   }
 
   static styles = css`
