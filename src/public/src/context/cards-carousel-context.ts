5107c7f260aa2910438dc5e2de167e87b3f33bba
Chege Gitau <d.chege711@gmail.com>
2024-04-30 08:07:27 -0700

    [Context] Create a CardsCarousel context

---
+++ src/public/src/context/cards-carousel-context.ts
@@ -0,0 +1,29 @@
+import { createContext } from '@lit/context';
+
+import { CardsCarousel } from '../CardsCarousel.js';
+
+export const kCardsCarouselUpdateCursorEventName = 'cards-carousel-update-cursor';
+
+export enum CardsCarouselUpdateCursorDirection {
+  Next = 1,
+  Previous = -1,
+}
+
+export class CardsCarouselUpdateCursorEvent extends Event {
+  direction: CardsCarouselUpdateCursorDirection;
+
+  constructor(direction: CardsCarouselUpdateCursorDirection) {
+    super(kCardsCarouselUpdateCursorEventName, { bubbles: true, composed: true });
+    this.direction = direction;
+  }
+}
+
+export const cardsCarouselContext = createContext<CardsCarousel>(
+  Symbol('cards-carousel-context')
+);
+
+declare global {
+  interface GlobalEventHandlersEventMap {
+    'cards-carousel-update-cursor': CardsCarouselUpdateCursorEvent;
+  }
+}
