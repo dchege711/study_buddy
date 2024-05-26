5dccbb7d68d46a07cb6ee8ce73dd1cc228c09b4d
Chege Gitau <d.chege711@gmail.com>
2024-05-26 14:00:32 -0700

    [cg-editable-card-title] Add component
    Communicates updates via CardChangedEvent

---
+++ src/public/src/components/card-viewer/components/editable-card-title.ts
@@ -0,0 +1,42 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import { CardChangedEvent } from './card-changed-event.js';
+
+@customElement('cg-editable-card-title')
+export class EditableCardTitle extends LitElement {
+  @property({ type: String}) value = '';
+
+  @query('input')
+  private input!: HTMLInputElement;
+
+  render() {
+    return html`
+      <input type='text' value=${this.value} @input=${this.inputChanged} />
+    `;
+  }
+
+  static styles = css`
+    :host {
+      flex-grow: 1;
+      display: flex;
+      flex-direction: column;
+    }
+
+    input {
+      padding: 8px;
+      border: none;
+      border-bottom: 1px solid var(--main-border-color);
+      font-weight: bolder;
+    }
+  `;
+
+  private inputChanged() {
+    this.dispatchEvent(new CardChangedEvent({ title: this.input.value }));
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'cg-editable-card-title': EditableCardTitle;
+  }
+}
