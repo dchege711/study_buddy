eec1fab75bf812f71ba50af5baf03af9301c5bbf
Chege Gitau <d.chege711@gmail.com>
2024-05-27 14:15:11 -0700

    Add a UserFacingError helper class

---
+++ src/errors.ts
@@ -0,0 +1,17 @@
+/**
+ * A helper class for errors that the user can overcome with the appropriate
+ * action.
+ */
+export class UserFacingError extends Error {
+  /** The URL where the user should be redirected to. */
+  redirectURL: string;
+
+  /** A message that will be shown to the user to help them fix the error. */
+  message: string;
+
+  constructor(message: string, redirectURL: string) {
+    super(message);
+    this.redirectURL = redirectURL;
+    this.message = message;
+  }
+}
