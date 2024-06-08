f3c67eca109f103844cc106eb18af5e8c96015b8
Chege Gitau <d.chege711@gmail.com>
2024-06-08 10:28:32 -0700

    Remove legacy code that doesn't translate to web components

--- src/public/src/legacy/SyntaxHighlighting.ts
+++
@@ -1,6 +0,0 @@
-// Will be overwritten in src/views/partials/syntax_highlighting_and_latex.ejs.
-let addSyntaxHighlightingImplementation = () => {};
-
-export function addSyntaxHighlighting() {
-  addSyntaxHighlightingImplementation();
-}
