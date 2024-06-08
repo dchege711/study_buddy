2eb6869cc5092e117dea2f54574e103aa4459483
Chege Gitau <d.chege711@gmail.com>
2024-06-08 10:28:32 -0700

    Remove legacy code that doesn't translate to web components

--- src/public/src/legacy/Latex.ts
+++
@@ -1,6 +0,0 @@
-// Will be overwritten in src/views/partials/syntax_highlighting_and_latex.ejs.
-let latexImplementation = (elementIDs: string[]) => {};
-
-export function renderLatex(elementIDs: string[]): void {
-  latexImplementation(elementIDs);
-}
