498b78045f268ddd1c44dd40a504f77a19db1461
Chege Gitau <d.chege711@gmail.com>
2024-06-09 20:33:23 -0700

    [ESLint] Comment current options in config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,28 @@
 /* eslint-env node */
+
+/**
+ * ESLint configuration for TypeScript projects.
+ *
+ * [1]: https://eslint.org/docs/latest/use/configure/configuration-files
+ */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  // Required, else ESLint with try to parse TS code as if it were regulatr JS.
+  parser: "@typescript-eslint/parser",
+
+  plugins: [
+    // Allows us to use typescript-eslint rules within this codebase.
+    "@typescript-eslint",
+  ],
+
+  extends: [
+    // ESLint's inbuilt recommended config. Small, sensible set of rules that
+    // lint for well-known best practices.
+    "eslint:recommended",
+
+    // Similar to `eslint:recommended`, but for TypeScript code.
+    "plugin:@typescript-eslint/recommended",
+  ],
+
+  // ESLint should not search beyond this directory for config files.
   root: true,
 };
