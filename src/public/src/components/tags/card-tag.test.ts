03eb0366f90d82d5e240813c189fbb24dad317bf
Chege Gitau <d.chege711@gmail.com>
2024-06-22 19:15:59 -0700

    [card-tag.test] Sample WC test

---
+++ src/public/src/components/tags/card-tag.test.ts
@@ -0,0 +1,10 @@
+import { expect, fixture, html } from "@open-wc/testing";
+
+import "./card-tag";
+
+describe("card-tag", () => {
+  it("should display the tag with prefix", async () => {
+    const tag = await fixture(html`<cg-card-tag .tag=${"test"}></cg-card-tag>`);
+    expect(tag.shadowRoot?.textContent?.trim()).to.equal("#test");
+  });
+});
