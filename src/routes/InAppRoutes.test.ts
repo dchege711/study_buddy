67e2459cb3277fd6f2a8af10993302e8233eb1c8
Chege Gitau <d.chege711@gmail.com>
2024-06-30 14:37:33 -0700

    [*.test] Refactor logInAgent to a common place

--- src/routes/InAppRoutes.test.ts
+++ src/routes/InAppRoutes.test.ts
@@ -1,38 +1,9 @@
-import { expect } from "chai";
 import { StatusCodes } from "http-status-codes";
 import request from "supertest";
 
-import { authenticateUser } from "../models/LogInUtilities";
 import { ACCOUNT, HOME, LOGIN, WIKI } from "../paths";
 import { app } from "../server";
-import { dummyAccountDetails } from "../tests/DummyAccountUtils";
-
-const authDetails = {
-  username_or_email: dummyAccountDetails.email,
-  password: dummyAccountDetails.password,
-};
-
-async function logInAgent() {
-  const agent = request.agent(app);
-
-  // Check that the user does exist.
-  const user = await authenticateUser(authDetails);
-  expect(user).to.not.be.null;
-
-  // Login, and follow redirect to `HOME`.
-  const result = await agent
-    .post(LOGIN)
-    .send(authDetails)
-    .redirects(1);
-
-  expect(result.status).to.equal(StatusCodes.OK);
-  expect(result.type).to.equal("text/html");
-
-  const finalURL = new URL(result.request.url);
-  expect(finalURL.pathname).to.equal(HOME);
-
-  return Promise.resolve(agent);
-}
+import { logInAgent } from "../tests/supertest.utils";
 
 describe(HOME, function() {
   it("requires authentication", function() {
