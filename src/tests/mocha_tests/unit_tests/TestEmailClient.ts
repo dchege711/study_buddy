7735a1f2b0ca54edbf0c104f2ac28c980a8ae04d
Chege Gitau <d.chege711@gmail.com>
2024-06-29 18:49:46 -0700

    [EmailClient] Mock the sendMail call to avoid pinging Mailgun

--- src/tests/mocha_tests/unit_tests/TestEmailClient.ts
+++ src/tests/mocha_tests/unit_tests/TestEmailClient.ts
@@ -4,17 +4,14 @@ import * as emailClient from "../../../models/EmailClient";
 
 import { APP_NAME, DEBUG_EMAIL_ADDRESS, EMAIL_ADDRESS } from "../../../config";
 
-describe("Test EmailClient\n", function() {
-  describe("Sending an email...", function() {
-    // TODO: This test should not hit the actual email server.
-    it.skip("should send an email successfully", function() {
-      return emailClient.sendEmail({
-        from: `${APP_NAME} <${EMAIL_ADDRESS}>`,
-        to: DEBUG_EMAIL_ADDRESS,
-        subject: "Testing EmailClient",
-        text: "Because there's HTML, this text won't appear in the email.",
-        html: "<button style='color:blue;'>The button does nothing</button>",
-      });
+describe("EmailClient\n", function() {
+  it("should send an email successfully", function() {
+    return emailClient.sendEmail({
+      from: `${APP_NAME} <${EMAIL_ADDRESS}>`,
+      to: DEBUG_EMAIL_ADDRESS,
+      subject: "Testing EmailClient",
+      text: "Because there's HTML, this text won't appear in the email.",
+      html: "<button style='color:blue;'>The button does nothing</button>",
     });
   });
 });
