52a987b1775d477d52fbb9c65ec34e14073324a0
Chege Gitau <d.chege711@gmail.com>
2024-06-29 17:39:37 -0700

    [InAppRoutes.test] E2E for an Express-powered auth-guarded page

--- src/routes/InAppRoutes.test.ts
+++ src/routes/InAppRoutes.test.ts
@@ -1,8 +1,55 @@
+import { expect } from "chai";
 import { StatusCodes } from "http-status-codes";
 import request from "supertest";
 
-import { WIKI } from "../paths";
+import { authenticateUser } from "../models/LogInUtilities";
+import { HOME, LOGIN, WIKI } from "../paths";
 import { app } from "../server";
+import { dummyAccountDetails } from "../tests/DummyAccountUtils";
+
+const authDetails = {
+  username_or_email: dummyAccountDetails.email,
+  password: dummyAccountDetails.password,
+};
+
+async function logInAgent() {
+  const agent = request.agent(app);
+
+  // Check that the user does exist.
+  const user = await authenticateUser(authDetails);
+  expect(user).to.not.be.null;
+
+  // Login, and follow redirect to `HOME`.
+  const result = await agent
+    .post(LOGIN)
+    .send(authDetails)
+    .redirects(1);
+
+  expect(result.status).to.equal(StatusCodes.OK);
+  expect(result.type).to.equal("text/html");
+
+  const finalURL = new URL(result.request.url);
+  expect(finalURL.pathname).to.equal(HOME);
+
+  return Promise.resolve(agent);
+}
+
+describe(HOME, function() {
+  it("requires authentication", function() {
+    return request(app)
+      .get(HOME)
+      .expect(StatusCodes.SEE_OTHER)
+      .expect("Location", LOGIN);
+  });
+
+  it("GET returns an HTML page", async function() {
+    const agent = await logInAgent();
+    return agent
+      .get(HOME)
+      .expect(StatusCodes.OK)
+      .expect("Content-Type", /html/);
+  });
+});
 
 describe(WIKI, function() {
   it("GET returns an HTML page", function() {
